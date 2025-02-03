06fdc745523db1b6069e8a550667541cf35dadab
nidacantik26 <nida442000@gmail.com>
2025-02-03 10:20:54 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:085378900002"><i class="fas fa-phone"></i>085378900002</a>
+         <a href="tel:086767202020"><i class="fas fa-phone"></i> 086767202020</a>
+         <a href="mailto:great_woman@gmail.com"><i class="fas fa-envelope"></i> great_woman@gmail.com</a>
+         <a href="mailto:cool_woman@gmail.com"><i class="fas fa-envelope"></i> cool_woman@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Indonesia Raya - Haurguelis </a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2025 by kelompok Qu4tr | Great Woman</div>
+
+</section>
+
+<!-- footer section ends -->
